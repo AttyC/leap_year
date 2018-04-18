27c76ffa67bff4684430305e437c2e6d769e1b9f
Antonia Cronin <antoniajcronin@gmail.com>
2018-04-18 21:44:22 +0100

    tests for loop with start year and number of repetitions

--- lib/leap.rb
+++ lib/leap.rb
@@ -1,10 +1,16 @@
+# understands how to calculate leap year
 class Leap
+  attr_reader :year, :start_year
+  attr_accessor :counter, :number_of_years
 
-  attr_reader :year
+  def initialize
+    @counter = 0
+    @start_year = 2018
+  end
 
   def leap_year?(year)
-    if (year %4 == 0 )
-      if (year %100) == 0 && (year %400 != 0)
+    if (year % 4).zero?
+      if (year % 100).zero? && (year % 400 != 0)
         false
       else
         true
@@ -13,4 +19,15 @@ class Leap
       false
     end
   end
+
+  def leap_loop(start_year, number_of_years)
+    while @counter < number_of_years do
+      if leap_year?(start_year)
+        @counter +=1
+        p start_year
+      end
+      start_year +=1
+    end
+  end
+
 end
--- spec/leapyear_spec.rb
+++ spec/leapyear_spec.rb
@@ -1,6 +1,6 @@
 describe Leap do
   subject(:leap) { described_class.new }
-  describe '#leap' do
+  describe '#leap_year?' do
     it 'returns true when year is divisible by 4' do
       expect(leap.leap_year?(2020)).to eq true
     end
@@ -13,6 +13,16 @@ describe Leap do
     end
     it 'returns false when year is divisible by 4 and 100 but not 400' do
       expect(leap.leap_year?(2100)).to eq false
+    end
+  end
+
+  describe '#leap_loop' do
+    it 'counter ends at 20' do
+      leap.leap_loop(2018, 20)
+      expect(leap.counter).to eq 20
+    end
+    it 'counter ends at 20' do
+      expect{leap.leap_loop(3000, 1)}.to output("3004\n").to_stdout
     end
   end
 end
